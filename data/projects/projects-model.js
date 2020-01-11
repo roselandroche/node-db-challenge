44ff6941e24934da3d8a2e46b6c606ad6fa1ce7f
roselandroche <r.landroche@gmail.com>
2020-01-10 21:27:19 -0500

    Trying to sort out endpoints

--- data/projects/projects-model.js
+++ data/projects/projects-model.js
@@ -12,26 +12,32 @@ function getProjects() {
     return db("projects").first()
 }
 
+function getProjectById(id) {
+    return db("projects").where({id}).first()
+}
+
 async function addProject(newProject) {
     const [id] = await db("projects").insert(newProject)
     return db("projects").where({ id }).first()
 }
 
-async function getTasks() {
+async function getTasks(project_id) {
     const tasks = await db("tasks as t")
         .join("projects as p", "t.projects_id", "p.id")
-        .select("p.name", "t.id", "t.description", "t.completed")
+        .select("p.name", "p.description", "t.id", "t.description", "t.completed")
+        .where("p.id", project_id)
     return tasks
 }
 
-function addTask() {
-
+function addTask(project_id) {
+    
 }
 
 module.exports = {
     getResources,
     addResource,
     getProjects,
+    getProjectById,
     addProject,
     getTasks,
     addTask,
--- data/projects/projects-router.js
+++ data/projects/projects-router.js
@@ -14,6 +14,23 @@ router.get("/projects", async (req, res, next) => {
     }
 })
 
+// get project by id
+router.get("/projects/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const project = await projectsModel.getProjectById(id)
+
+        if (project) {
+            res.json(project)
+        } else {
+            res.status(404).json({ message: "Project does not exist"})
+        }
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
 // add a project
 router.post("/projects", async (req, res, next) => {
     try {
@@ -25,10 +42,43 @@ router.post("/projects", async (req, res, next) => {
     }
 })
 
-// get tasks
-router.get("/tasks", async (req, res, next) => {
+// get tasks by id
+router.get("/tasks/:id", async (req, res, next) => {
+    const { id } = req.params;
     try {
-        res.json(await projectsModel.getTasks())
+        res.json(await projectsModel.getTasks(id))
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+// add task
+router.post("/tasks/:id", (req, res, next) => {
+    const newTask = req.body;
+    const { id } = req.params;
+
+    projectsModel.getProjectById(id)
+    .then(project => {
+        if(project) {
+            projectsModel.addTask(newTask, id)
+            .then(addedTask => {
+                res.status(201).json(addedTask)
+            })
+        } else {
+            res.status(404).json({message: "Invalid project"})
+        }
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+// get resources by id
+router.get("/resources/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        res.json(await projectsModel.getResources(id))
     }
     catch(err) {
         next(err)
